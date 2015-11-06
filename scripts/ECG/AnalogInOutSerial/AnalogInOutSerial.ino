884eff6c946b4a7014eeb9ca9654bbc6ff739b6c
Mohammad <mfarhan102@gmail.com>
2015-11-06 14:07:34 -0500

    modified arduino print to serial, modified ECG plotter

--- scripts/ECG/AnalogInOutSerial/AnalogInOutSerial.ino
+++ scripts/ECG/AnalogInOutSerial/AnalogInOutSerial.ino
@@ -1,31 +1,9 @@
-/*
-  Analog input, analog output, serial output
 
- Reads an analog input pin, maps the result to a range from 0 to 255
- and uses the result to set the pulsewidth modulation (PWM) of an output pin.
- Also prints the results to the serial monitor.
-
- The circuit:
- * potentiometer connected to analog pin 0.
-   Center pin of the potentiometer goes to the analog pin.
-   side pins of the potentiometer go to +5V and ground
- * LED connected from digital pin 9 to ground
-
- created 29 Dec. 2008
- modified 9 Apr 2012
- by Tom Igoe
-
- This example code is in the public domain.
-
- */
-
-// These constants won't change.  They're used to give names
-// to the pins used:
 const int analogInPin = A1;  // Analog input pin that the potentiometer is attached to
 
 
 int sensorValue = 0;        // value read from the pot
-int outputValue = 0;        // value output to the PWM (analog out)
+put to the PWM (analog out)
 
 void setup() {
   // initialize serial communications at 9600 bps:
@@ -35,15 +13,9 @@ void setup() {
 void loop() {
   // read the analog in value:
   sensorValue = analogRead(analogInPin);
-  // map it to the range of the analog out:
-
 
   // print the results to the serial monitor:
-  Serial.print("sensor = " );
-  Serial.print(sensorValue);
-Serial.print("\n " );
+  Serial.println(sensorValue);
   // wait 2 milliseconds before the next loop
-  // for the analog-to-digital converter to settle
-  // after the last reading:
-  delay(10);
+  delay(2);
 }
